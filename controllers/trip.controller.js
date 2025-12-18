0003754ed96395cbae7385db018442871b31f87e
Guillaume Lebegue <lebegue.guillaume26@gmail.com>
2025-12-18 15:39:07 +0100

    finish route

--- controllers/trip.controller.js
+++ controllers/trip.controller.js
@@ -1,15 +1,51 @@
-const { StatusCodes } = require("http-status-codes");
+const { StatusCodes, getStatusCode } = require("http-status-codes");
 const Trip = require("../models/Trip");
+const { categoryCodes, tags } = require("../helpers/data");
 
 //endpoints pour front :
 const getAll = async (req, res) => {
   const params = req.query;
   let formattedParams = {};
-  if (params.region) {
+  if (params.region && params.region !== "0") {
+    formattedParams.duration = parseInt(params.duration);
+  }
+  if (params.duration && params.duration !== "0") {
     formattedParams.region = parseInt(params.region);
+  }
+  if (params.town) {
+    formattedParams.town = { $regex: params.town, $options: "i" };
+  }
+  if (params.price) {
+    formattedParams.adultPrice = { $lte: params.price };
+  }
+  if (params.category && params.category !== "0") {
+    const category = categoryCodes.find(
+      (cat) => cat.code === parseInt(params.category)
+    );
+    if (category) {
+      formattedParams.category = category.name;
+    }
+  }
+  if (params.tags && params.tags !== "0") {
+    const tags = tags.find((tag) => tag.code === parseInt(params.tags));
+    if (category) {
+      formattedParams.tags = tag.name;
+    }
   }
   try {
     const trips = await Trip.find(formattedParams);
+    return res.status(StatusCodes.OK).send(trips);
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .send("Error while fetching trips");
+  }
+};
+
+const getAllBestsellers = async (req, res) => {
+  try {
+    const trips = await Trip.find({ tags: "bestseller" });
     return res.status(StatusCodes.OK).send(trips);
   } catch (error) {
     console.log(error);
@@ -36,5 +72,80 @@ const create = async (req, res) => {
       .send("Creation failed");
   }
 };
-
-module.exports = { create, getAll };
+const getOne = async (req, res) => {
+  const id = req.params.id;
+  if (!id) {
+    return res.status(StatusCodes.BAD_REQUEST).send("No ID provided");
+  }
+  try {
+    const trip = await Trip.findById(id);
+    if (!trip) {
+      return res.status(StatusCodes.BAD_REQUEST).send("No match");
+    }
+    return res.status(StatusCodes.OK).send(trip);
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .send("Error while retrieving the trip");
+  }
+};
+const patchOne = async (req, res) => {
+  const id = req.params.id;
+  if (!id) {
+    return res.status(StatusCodes.BAD_REQUEST).send("No ID provided");
+  }
+  try {
+    const trip = await Trip.findByIdAndUpdate(id, req.body, { new: true });
+    if (!trip) {
+      return res.status(StatusCodes.NOT_FOUND).send("No resource found");
+    }
+    return res.status(StatusCodes.OK).send(trip);
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .send("Error while patching the trip");
+  }
+};
+const deleteOne = async (req, res) => {
+  const id = req.params.id;
+  if (!id) {
+    return res.status(StatusCodes.BAD_REQUEST).send("No ID provided");
+  }
+  try {
+    const trip = await Trip.findByIdAndDelete(id);
+    if (!trip) {
+      return res.status(StatusCodes.NOT_FOUND).send("Nothing to delete");
+    }
+    return res.status(StatusCodes.OK).send(trip);
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .send("Error while deleting the trip");
+  }
+};
+const deleteAll = async (req, res) => {
+  try {
+    const result = await Trip.deleteMany();
+    if (result.deletedCount === 0) {
+      return res.status(StatusCodes.NOT_FOUND).send("Nothing to delete");
+    }
+    return res.status(StatusCodes.OK).send("all Deleted");
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .send("Error while deleting the trips");
+  }
+};
+module.exports = {
+  create,
+  getAll,
+  getOne,
+  getAllBestsellers,
+  patchOne,
+  deleteOne,
+  deleteAll,
+};
--- routes/trips.routes.js
+++ routes/trips.routes.js
@@ -3,7 +3,13 @@ const router = express.Router();
 const tripController = require("../controllers/trip.controller");
 //front
 router.get("/", tripController.getAll);
+router.get("/bestseller", tripController.getAllBestsellers);
 //postman
 
 router.post("/", tripController.create);
+router.get("/:id", tripController.getOne);
+router.patch("/:id", tripController.patchOne);
+router.delete("/:id", tripController.deleteOne);
+router.delete("/", tripController.deleteAll);
+
 module.exports = router;
