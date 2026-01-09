075310db84a460ba0f07e1e666ae97e49a25d61f
Guillaume Lebegue <lebegue.guillaume26@gmail.com>
2026-01-09 19:21:27 +0100

    config vercel

---
+++ api/index.js
@@ -0,0 +1,108 @@
+const express = require("express");
+const morgan = require("morgan");
+const connectToDatabase = require("../database");
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const helmet = require("helmet");
+const expressRateLimit = require("express-rate-limit");
+const xssMiddleware = require("../middlewares/xssMiddleware");
+const mongoSanitize = require("express-mongo-sanitize");
+//routes:
+const orderRoutes = require("../routes/order.routes");
+const advisorRoutes = require("../routes/advisor.routes");
+const agencyRoutes = require("../routes/agency.routes");
+const tripRoutes = require("../routes/trips.routes");
+const authRoutes = require("../routes/auth.routes");
+const profileRoutes = require("../routes/profile.routes");
+const checkoutRoutes = require("../routes/checkout.routes");
+const { StatusCodes } = require("http-status-codes");
+
+//instance
+const app = express();
+const port = 5137;
+//config
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+dotenv.config();
+app.use(helmet());
+app.use(express.json({ limit: "100kb" }));
+app.use(express.urlencoded({ extended: true, limit: "100kb" }));
+app.use(xssMiddleware);
+app.set("trust proxy, 1"); // Allows vercel headers
+app.use((req, res, next) => {
+  Object.defineProperty(req, "query", {
+    value: { ...req.query },
+    writable: true,
+    configurable: true,
+    enumerable: true,
+  });
+  next();
+});
+
+app.use(mongoSanitize());
+
+// Global security
+//confg rate limit
+const limitOptions = {
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  handler: (req, res) => {
+    res
+      .status(StatusCodes.TOO_MANY_REQUESTS)
+      .json({ status: 429, error: "Too many requests" });
+  },
+  standardHeaders: true,
+  legacyheaders: false,
+};
+app.use(expressRateLimit(limitOptions));
+//cors config
+const allowedOrigins = [
+  "http://localhost:5173", // Vite (front)
+  process.env.CLIENT_URL_PRODUCTION, // Prod (si défini)
+].filter(Boolean);
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true); // Postman/curl
+      if (allowedOrigins.includes(origin)) return callback(null, true);
+      return callback(new Error(`Not allowed by CORS: ${origin}`));
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "X-Forwarded-For"],
+  })
+);
+connectToDatabase();
+
+//config multer
+app.locals.uploader = multer({
+  storage: multer.memoryStorage({}),
+  limits: { fileSize: 10 * 1024 * 1024 },
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype.startsWith("image")) {
+      cb(null, true);
+    } else {
+      cb(new Error("Only images are accepted"));
+    }
+  }, //Maw size : 10Mb
+});
+//endpoints
+app.use("/orders", orderRoutes);
+app.use("/advisors", advisorRoutes);
+app.use("/agencies", agencyRoutes);
+app.use("/trips", tripRoutes);
+app.use("/auth", authRoutes);
+app.use("/profile", profileRoutes);
+app.use("/create-checkout-session", checkoutRoutes);
+app.use((req, res) => {
+  return res.status(404).send("Page note found");
+});
+app.listen(port, () => {
+  console.log(`Hexatrip server running on port: ${port}`);
+});
+console.log("RUNNING FILE =", __filename);
--- database.js
+++ database.js
@@ -3,16 +3,20 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const mongoUriAtlas = process.env.MONGODB_URI_ATLAS;
+const mongoUriAtlas = `mongodb+srv://Guimaraiche:${DB_PWD}@cluster0.bmzuau6.mongodb.net/?retryWrites=true&w=majority&appName=hexa-trip`;
 const mongoUriLocalhost =
   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/hexa-trip";
 
 const connectToDatabase = async () => {
   const isProduction = process.env.NODE_ENV === "production";
-  const mongoUri = isProduction ? mongoUriAtlas || mongoUriLocalhost : mongoUriLocalhost;
+  const mongoUri = isProduction
+    ? mongoUriAtlas || mongoUriLocalhost
+    : mongoUriLocalhost;
 
   if (!mongoUri) {
-    console.error("MongoDB URI is missing. Set MONGODB_URI (and MONGODB_URI_ATLAS for prod).");
+    console.error(
+      "MongoDB URI is missing. Set MONGODB_URI (and MONGODB_URI_ATLAS for prod)."
+    );
     return;
   }
 
